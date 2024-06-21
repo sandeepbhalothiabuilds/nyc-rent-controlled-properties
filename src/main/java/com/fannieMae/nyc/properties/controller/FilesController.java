e4c43d911037bd1d6f66d4d77a092bc3e5d6f90e
lekshmimohansctce <lekshmimohansctce@gmail.com>
2024-06-21 13:55:54 -0500

    Update FilesController.java - health endpoint

--- src/main/java/com/fannieMae/nyc/properties/controller/FilesController.java
+++ src/main/java/com/fannieMae/nyc/properties/controller/FilesController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -32,6 +33,11 @@ public class FilesController {
             message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
+    }
+
+      @GetMapping("/") public String health()
+    {
+        return "***** Health Check ****";
     }
 
 }
